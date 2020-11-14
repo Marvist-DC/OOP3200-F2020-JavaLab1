6b55ab98ca84c34a47cfce00fac2092bfedb6249
Domenic Catalano <domenic.catalano@dcmail.ca>
2020-11-14 15:48:43 -0500

    Final

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -23,33 +23,34 @@ public class Main
         final int HIGHEST_SCORE = 300;
         int[][] scores = new int[NUM_OF_GAMES][NUM_OF_USERS];
         String[] playerNames = {"Tom", "Tan", "Domenic"};
-        boolean isValidInput = false;
 
         for (int round = 0; round < NUM_OF_GAMES; ++round)
         {
             for (int player = 0; player < NUM_OF_USERS; ++player)
             {
-                System.out.printf("Please enter %s's score for GAME #%x: ", playerNames[player], round);
+                boolean isValidInput = false;
                 while(!isValidInput)
                 {
-
-
-                    isValidInput = true;
-
                     try
                     {
-                        if(( < LOWEST_SCORE) || ( > HIGHEST_SCORE))
+                        int tempScore = 0;
+                        System.out.printf("Please enter %s's score for GAME #%d: ", playerNames[player], (round + 1));
+                        tempScore = keyboard.nextInt();
+                        if(tempScore < LOWEST_SCORE || tempScore > HIGHEST_SCORE)
                         {
-                            System.out.printf("Error: You must enter a score between %x and %x inclusive", LOWEST_SCORE, HIGHEST_SCORE);
+                            System.out.printf("Error: You must enter a score between %d and %d inclusive", LOWEST_SCORE, HIGHEST_SCORE);
                             keyboard.nextLine();
-                            isValidInput = false;
+                        }
+                        else
+                        {
+                            scores[round][player] = tempScore;
+                            isValidInput = true;
                         }
                     }
                     catch (InputMismatchException inputMismatchException)
                     {
                         System.out.println("Error: You must enter a whole integer value");
                         keyboard.nextLine();
-                        isValidInput = false;
                     }
                 }
             }
@@ -58,16 +59,15 @@ public class Main
         // Output results
         for (int player = 0; player < NUM_OF_USERS; ++player)
         {
-            System.out.println("\nScore Details for " + playerNames[player]);
+            System.out.printf("\nScore Details for %s:\n", playerNames[player]);
             int tempTotal = 0;
-            for (int game = 0; game < NUM_OF_GAMES; ++game)
+            for (int round = 0; round < NUM_OF_GAMES; ++round)
             {
-                System.out.print("Game #" + (game + 1) + ": " + scores[player][game] + "\n");
-                tempTotal += scores[player][game];
-                if (game == 1)
+                System.out.printf("Game #%d: %d\n", (round + 1), scores[round][player]);
+                tempTotal += scores[round][player];
+                if (round == 1)
                 {
-                    DecimalFormat df = new DecimalFormat("#.#");
-                    System.out.println("Average for " + playerNames[player] + ": " + df.format(tempTotal / 2));
+                    System.out.printf("Average for %s: %.1f\n", playerNames[player], (float)(tempTotal / 2));
                 }
             }
         }
